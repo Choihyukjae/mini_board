618a0e26441a4b42fabe45a297f03ddf23bf6fcf
Choihyukjae <wo4013@naver.com>
2023-04-10 11:47:30 +0900

    doc\sql

---
+++ doc/sql/create_01_board.sql
@@ -0,0 +1,7 @@
+CREATE TABLE board_info(
+board_no INT PRIMARY KEY AUTO_INCREMENT 
+,board_title VARCHAR(100) NOT NULL
+,board_contents VARCHAR(1000) NOT NULL 
+,board_write_date DATETIME NOT NULL
+,board_del_flg CHAR(1) default ('0') NOT NULL
+,board_del_date DATETIME );
